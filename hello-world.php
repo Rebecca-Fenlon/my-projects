12fc5cc1de80fe58e617f8a87f6cbd238dd5c048
Rebecca-Fenlon <sa.rebecca94@gmail.com>
2016-10-31 21:59:34 +0000

    Updated Files

--- hello-world.php
+++ hello-world.php
@@ -1,12 +1,23 @@
 <html>
 <head>
+<style>
+body{
+	background-color: #b3b3b3
+}
+</style>
+
 <title>Hello World</title>
 </head>
 
 <body>
 <h1>Hello World!</h1>
 
+<img src="https://media.giphy.com/media/Dm5tILmumdouA/giphy.gif" alt="beyonce hello gif" style="width:304px;height:228px;">
 
+<p><a href="http://student.computing.dcu.ie/~fenlonr2/">3rd Year GitLab Project</a></p>
+
+<p>LinkedIn: <a href="https://www.linkedin.com/in/rebecca-a-fenlon-85903612b?trk=nav_responsive_tab_profile">
+<img border="0" alt="LinkedIn" src="http://www.freeiconspng.com/uploads/linkedin-logo-1.png" width="40" height="40"></a></p>
 
 
 </body>
